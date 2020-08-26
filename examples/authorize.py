af1df4173399f3270cdb7059d5a381f8acfd9321
Andrew Prokofieff <65781959+andprokofieff@users.noreply.github.com>
2020-08-26 20:46:37 +0500

    Added comments

--- examples/authorize.py
+++ examples/authorize.py
@@ -1,6 +1,6 @@
 import apigram
 
-bot = apigram.session('token')
+bot = apigram.session('123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11')
 api = apigram.get_api(bot)
 
 # How to get bot's info via simplest method getMe(): https://core.telegram.org/bots/api#getme
